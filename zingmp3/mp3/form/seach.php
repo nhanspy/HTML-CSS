7297ecbde057c2daadb8c3aeb067735929073a72
nhanspy <nhan0095@gmail.com>
2018-01-15 16:13:03 +0700

    lan 1

---
+++ zingmp3/mp3/form/seach.php
@@ -0,0 +1,11 @@
+<div class="search_new">
+	<div class="search_new_mid">
+		<form action="./?mod=timkiem" method="post">
+		<input class="tbsearch" type="text" name="key" onclick="this.select()" onmouseover="this.focus()" onkeyup="M.sea(this.value)" maxlength="255" placeholder="Tìm bài hát ở đây ^_^ (PN)">
+		<input type="submit" value="" id="btn_search" name="nuttim">
+		</form>
+	</div>
+</div>
+<div class="tab">
+    <li class="tab_selected"><a title="Lớp LT04D Trường Cao Đẳng CNTT Hữu Nghị Việt - Hàn">Nhóm 7 | CCLT04D</a></li>
+</div>
